d14dfb5438347ae39d1cc0933798c4ce22b0a954
nlumiere <nicholaslumiere@gmail.com>
2022-04-21 15:04:45 -0600

    commenting and organization

--- kalman/given_kalman.py
+++ kalman/given_kalman.py
@@ -1,12 +1,14 @@
+# Wrapper for cv2 kalman filter adapted from code at https://pysource.com/
+
 import cv2
 import numpy as np
-
 
 class KalmanFilter:
     kf = cv2.KalmanFilter(4, 2)
     kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
     kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
 
+    # takes in points (x,y) as input, predicts the next point 
     def predict(self, coordX, coordY):
         measured = np.array([[np.float32(coordX)], [np.float32(coordY)]])
         self.kf.correct(measured)
--- kalman/kalman.py
+++ kalman/kalman.py
@@ -2,6 +2,11 @@ import numpy as np
 
 class KalmanFilter():
 
+    # sample initialization:
+    # x_init = np.zeros(6)
+    # P_init = np.diag(np.full(6, 500))
+    # R_init = np.array([[9,0],[0,9]])
+    # kf = KalmanFilter(1, x_init, P_init, R_init, 0.2**2)
     def __init__(self, timestep, x_init, P_init, R_init, var_a, gain=1.):
         self.x = x_init
         self.P = P_init
@@ -11,7 +16,7 @@ class KalmanFilter():
         self.H = self.construct_observation_matrix()
         self.gain = gain
 
-    # initialization equations
+    # From https://www.kalmanfilter.net/multiExamples.html
     def construct_state_transition_matrix(self, t):
         return np.array([
         [1, t, 0.5*t**2, 0, 0, 0],
@@ -22,6 +27,7 @@ class KalmanFilter():
         [0, 0, 0, 0, 0, 1]
         ])
 
+    # From https://www.kalmanfilter.net/multiExamples.html
     def construct_process_noise_matrix(self, t, var_a):
         return np.array([
         [t**4/4, t**3/2, t**2/2, 0, 0, 0],
@@ -32,26 +38,31 @@ class KalmanFilter():
         [0, 0, 0, t**2/2, t, 1]
         ])*var_a
 
+    # From https://www.kalmanfilter.net/multiExamples.html
     def construct_observation_matrix(self):
         return np.array([
         [1, 0, 0, 0, 0, 0],
         [0, 0, 0, 1, 0, 0]
         ])
 
-    # update equations
+    # returns transition matrix times position vector
     def state_extrapolation(self):
         x_hat = np.matmul(self.F, self.x)
         return x_hat
 
+    # extrapolates covariance from transition matrix and covarience
     def covariance_extrapolation(self):
         return np.matmul(self.F, np.matmul(self.P, self.F.T))
 
+    # computes kalman gain
     def compute_kalman_gain(self):
         return np.matmul(self.P, np.matmul(self.H.T, np.linalg.inv(np.matmul(self.H, np.matmul(self.P, self.H.T)) + self.R)))
 
+    # updates location vector based on new observation 'z'
     def state_update(self, z):
         return self.x + np.matmul(self.compute_kalman_gain(), (z - np.matmul(self.H, self.x)))
 
+    # updates covariance
     def covariance_update(self):
         K = self.compute_kalman_gain()*self.gain
         P = np.matmul(np.eye(K.shape[0]) - np.matmul(K, self.H), np.matmul(self.P, (np.eye(K.shape[0]) - np.matmul(K, self.H)).T)) + np.matmul(K, np.matmul(self.R, K.T))
@@ -60,6 +71,7 @@ class KalmanFilter():
             return self.P
         else: return P
 
+    # takes in a new observation and performs one 'iteration' of the kalman filter
     def run(self, z):
 
         self.x = self.state_update(z)
@@ -72,6 +84,7 @@ class KalmanFilter():
 
         return self.x[0], self.x[3]
 
+    # extrapolates one kalman filter iteration without a new observation
     def predict(self):
 
         self.x = self.state_extrapolation()
